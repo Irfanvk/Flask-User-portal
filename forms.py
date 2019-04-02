19963bec78e4fc6ee474d1016f81bd4d2bf49e43
irfanvk <ahamad.irfa@gmail.com>
2019-04-02 17:26:57 +0530

    login and logout service added

--- forms.py
+++ forms.py
@@ -9,9 +9,21 @@ from flask_wtf import FlaskForm
 
 class SignupForm(FlaskForm):
     """docstring for SignupForm"""
-    first_name = StringField('First name', validators=[DataRequired("please enter the first name")])
-    last_name = StringField('Last name', validators=[DataRequired("please enter the last name")])
-    email = StringField('Email', validators=[DataRequired("please enter the email address"),Email("please enter your email address")])
+    first_name = StringField('First name', validators=[
+                             DataRequired("please enter the first name")])
+    last_name = StringField('Last name', validators=[
+                            DataRequired("please enter the last name")])
+    email = StringField('Email', validators=[DataRequired(
+        "please enter the email address"), Email("please enter your email address")])
     password = PasswordField('Password', validators=[
                              DataRequired("Please enter a password"), Length(min=6, max=12, message="password must be atleast 6 characters")])
     submit = SubmitField('Sign up')
+
+
+class LoginForm(FlaskForm):
+    """docstring for SignupForm"""
+    email = StringField('Email', validators=[DataRequired(
+        "please enter the email address"), Email("please enter your email address")])
+    password = PasswordField('Password', validators=[
+                             DataRequired("Please enter a password"), Length(min=6, max=12, message="password must be atleast 6 characters")])
+    submit = SubmitField('Login')
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, escape, url_for
 from models import db, User
-from forms import SignupForm
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 # 'postgresql://postgres:postgres@localhost:5432/flask_1'
@@ -8,11 +8,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/flask_1'
 db.init_app(app)
 
-app.secret_key = "deelopment-key"
+app.secret_key = "development-key"
 
 
 @app.route("/")
 def index():
+    # if email in session:
+    #     return 'Logged in as %s' % escape(session['email'])
+    # return 'You are Logged in'
     return render_template("index.html")
 
 
@@ -33,9 +36,45 @@ def signup():
                            form.email.data, form.password.data)
             db.session.add(newuser)
             db.session.commit()
-            return "Success!"
+
+            session['email'] = newuser.email
+            return redirect(url_for('home'))
     elif request.method == 'GET':
         return render_template('signup.html', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('login.html')
+        else:
+            email = form.email.data
+            password = form.password.data
+
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                session['email'] = form.email.data
+                print(session)
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('login'))
+
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
+
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
